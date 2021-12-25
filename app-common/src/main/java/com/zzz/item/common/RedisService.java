6f70200a2252c68e53f2dfc65c71c240ce04faae
zhangxiaokang <15827444365@163.com>
2021-12-25 13:19:54 +0800

    微服务初始化

---
+++ app-common/src/main/java/com/zzz/item/common/RedisService.java
@@ -0,0 +1,10 @@
+package com.zzz.item.common;
+
+/**
+ * 功能描述:
+ *
+ * @Auth: zhangXiaoKang
+ * @Date: 2021-12-2021/12/25
+ */
+public class RedisService {
+}
---
+++ app-common/src/main/java/com/zzz/item/common/io/CommonRequest.java
@@ -0,0 +1,17 @@
+package com.zzz.item.common.io;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 功能描述:
+ *
+ * @Auth: zhangXiaoKang
+ * @Date: 2021-09-13
+ */
+
+@Setter
+@Getter
+public class CommonRequest {
+    private String commonId;
+}
